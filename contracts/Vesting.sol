a02b7202875ee18e6c7784ab5fc399571d6b1b2e
OptimusOpus <taylor.scheel@dltx.io>
2022-08-03 15:10:20 +1000

    Add tests

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,194 +1,265 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-pragma abicoder v2;
+pragma solidity 0.8.15;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Vesting is Ownable {
+struct VestingDetail {
+    //Vestor address
+    address beneficiary;
+    // Amount to be vested.
+    uint256 vestingAmount;
+    // Vesting duration, after cliff.
+    uint256 duration;
+    // Amount already claimed by beneficiary.
+    uint256 claimedAmount;
+    // Time at which beneficiary last claimed.
+    uint256 lastClaimedTime;
+    // Initial amount to be claimed, included in vestingAmount.
+    uint256 initialAmount;
+    // Whether the initialAmount value was claimed.
+    bool initialClaimed;
+    // Time at which vesting begins.
+    uint256 claimStartTime;
+}
+
+interface IInvestor {
+    function setVesting(VestingDetail[] calldata _vestingDetails) external;
+    function claim() external;
+}
+
+// Client's requirement not yet finalized, still waiting.
+contract InvestorVesting is IInvestor, Ownable {
     using SafeERC20 for ERC20;
 
-    address public token; //= 0x8Ef9B898db563d3c6175c2CDdfCe5027C36380fc;
-    uint256 private immutable start;
-    // NOTE: Don't forget to add this contract address to the whitelisted addresses
-    address public constant funding_seed1 =
-        0x73C2006262Ab12962120BEC17d1815A0EB845e76;
-    address public constant paulAntonelli =
-        0xfB98a42A0eb71470A9801e10E64B69f5A68e0024;
-    address public constant dev =
-        0xfa0B65413E3E81FAF7321f85b2AfdD0EffF13Ef5;
-    address public constant team =
-        0xb5408c2977daa3E371279b72bD598000db84d12a;
-    address public constant future =
-        0xACf1B2B00801Dd725Ee5393dd1E24ea74dDfBCEB;
-    address public constant incentives =
-        0x6A378aFDd6Fc4387791992aA893FC6F9c7EC258b;
-    address public constant intExpansion =
-        0x177ccDec7aDaaC034b05C7F35Af9fe325928a456;
-    address public constant treasury =
-        0x470D968F0d27075F3Db0f28d2C2a5a6EEbaD9E65;
+    address public immutable token;
+    uint256 public startDate;
+    uint256 public totalClaimed;
+    uint256 public totalVestingAmount;
+    //Maximum number of vesting details
+    uint256 public maxVestingDetailArray;
 
-    // Total amount of funds to be vested for each user/use case
-    uint256 public constant funding_seed1VestingCap = 250_000_000 * 1e18;
-    uint256 public constant paulAntonelliVestingCap = 50_000_000 * 1e18;
-    uint256 public constant devVestingCap = 350_000_000 * 1e18;
-    uint256 public constant teamVestingCap = 1_000_000_000 * 1e18;
-    uint256 public constant futureVestingCap = 2_000_000_000 * 1e18;
-    uint256 public constant incentivesVestingCap = 600_000_000 * 1e18;
-    uint256 public constant intExpansionVestingCap = 500_000_000 * 1e18;
-    uint256 public constant treasuryVestingCap = 200_000_000 * 1e18;
+    address public operator;
 
-    // Vesting intervals
-    uint256 public constant funding_seed1VestingTime = 7 days;
-    uint256 public constant paulAntonelliVestingTime = 7 days;
-    uint256 public constant devVestingTime = 7 days;
-    uint256 public constant teamVestingTime = 26 weeks;
-    uint256 public constant futureVestingTime = 4 weeks;
-    uint256 public constant incentivesVestingTime = 7 days;
-    uint256 public constant intExpansionVestingTime = 4 weeks;
-    uint256 public constant treasuryVestingTime = 7 days;
+    /// @dev event for set start date
+    /// @param date The date of the start date
+    event StartDateSet(uint256 date);
 
-    // Vesting amount per interval
-    uint256 public constant funding_seed1VestingAmount = 4_326_924;
-    uint256 public constant paulAntonelliVestingAmount = 865_385;
-    uint256 public constant devVestingAmount = 3_365_385;
-    uint256 public constant teamVestingAmount = 250_000_000;
-    uint256 public constant futureVestingAmount = 30_000_000;
-    uint256 public constant incentivesVestingAmount = 5_192_308;
-    uint256 public constant intExpansionVestingAmount = 8_333_334;
-    uint256 public constant treasuryVestingAmount = 1_730_770;
+    /// @dev event when beneficiary claim tokens
+    /// @param beneficiary a beneficiary address
+    /// @param amount a claimed amount
+    event Claimed(address indexed beneficiary, uint256 amount);
 
-    // Vesting amount for first interval
-    uint256 public constant funding_seed1InitVesting = 25_000_000;
-    uint256 public constant paulAntonelliInitVesting = 5_000_000;
-    uint256 public constant devInitVesting = devVestingAmount;
-    uint256 public constant teamInitVesting = teamVestingAmount;
-    uint256 public constant futureInitVesting = 200_000_000;
-    uint256 public constant incentivesInitVesting = 60_000_000;
-    uint256 public constant intExpansionInitVesting = intExpansionVestingAmount;
-    uint256 public constant treasuryInitVesting = 20_000_000;
+    /// @dev event when beneficiary's vesting detail been set
+    /// @param beneficiary a beneficiary address
+    /// @param amount a claimed amount
+    event Vested(address indexed beneficiary, uint256 amount);
 
-    mapping(address => uint256) public lastClaim;
-    mapping(address => uint256) public claimed;
+    modifier onlyOperator() {
+        require(
+            msg.sender == operator,
+            "onlyOperator: caller is not the operator"
+        );
+        _;
+    }
 
-    constructor(address _token, uint256 _start) {
-        require(_token != address(0), "Token address cannot be 0");
-        uint256 startTime = _start;
-        start = startTime;
-        lastClaim[dev] = startTime;
-        lastClaim[team] = startTime;
-        lastClaim[intExpansion] = startTime;
-        // No claims for 6 months, and claims every week
-        lastClaim[future] = startTime + 175 days;
-
-        // Turn back vesting time to allow initial vesting
-        lastClaim[funding_seed1] = startTime - 7 days;
-        lastClaim[paulAntonelli] = startTime - 7 days;
-        lastClaim[incentives] = startTime - 7 days;
-        lastClaim[treasury] = startTime - 7 days;
+    constructor(address _token, uint256 _startDate, uint256 _maxVestingDetailArray) {
+        require(
+            _startDate >= block.timestamp,
+            "Start date cannot be before the deployment date"
+        );
+        require(_token != address(0), "Address cannot be zero");
+        startDate = _startDate;
         token = _token;
+        maxVestingDetailArray = _maxVestingDetailArray;
+        operator = msg.sender;
+
+        //emit event
+        emit StartDateSet(_startDate);
     }
 
-    function _vestCalc(address who, uint256 initialVestingAmount, uint vestingAmount, uint256 vestingCap) private returns (bool) {
-        require(who != address(0), "Cannot claim from address 0");
-        require(initialVestingAmount > 0, "Cannot claim 0 tokens");
-        require(initialVestingAmount <= vestingCap, "Cannot claim more than vesting cap");
+    mapping(address => VestingDetail) internal vestingDetails;
 
-        uint256 claimedAmount;
-        if (claimed[who] == 0) {
-            // First Vesting Claim
-            claimedAmount = initialVestingAmount * 1e18;
-        } 
-        // If the claim amount is greater than the remaining amount owed, return the remaining amount owed
-        else if (claimed[who] + (vestingAmount * 1e18) > vestingCap) {
-            uint256 finalClaim = vestingCap - claimed[who];
-            claimedAmount = finalClaim;
-        } 
-        // Otherwise, return the standard vesting amount
-        else {
-            claimedAmount = vestingAmount * 1e18;
+    /**
+     * @dev Allow owner set user's vesting struct
+     * @param _vestingDetails A list of beneficiary's vesting.
+     */
+
+    function setVesting(VestingDetail[] calldata _vestingDetails)
+        override
+        external
+        onlyOperator
+    {
+        uint256 count = _vestingDetails.length;
+        // At least one vesting detail is required.
+        require(count > 0, "No vesting list provided");
+        // Check on the maximum size over which the for loop will run over.
+        require(count <= maxVestingDetailArray, "Too many vesting details");
+
+        for (uint256 i = 0; i < count; i++) {
+            address beneficiary = _vestingDetails[i].beneficiary;
+            require(
+                beneficiary != owner() &&
+                    beneficiary != address(0) &&
+                    beneficiary != operator,
+                "Beneficiary address is not valid"
+            );
+            //Check if beneficiary already has a vesting
+            require(
+                vestingDetails[beneficiary].vestingAmount == 0,
+                "Vesting already exists"
+            );
+            //Beneficiary's vesting amount must be greater than 0
+            require(
+                _vestingDetails[i].vestingAmount > 0,
+                "Vesting amount is not valid"
+            );
+            //Vesting duration must be greater than 0
+            require(_vestingDetails[i].duration > 0, "Duration is not valid");
+            //New beneficiary's initial claimed amount must be 0
+            require(
+                _vestingDetails[i].claimedAmount == 0,
+                "Claimed amount is not valid"
+            );
+            //New beneficiary's last claimed time must be 0,indicating that they have never claimed
+            require(
+                _vestingDetails[i].lastClaimedTime == 0,
+                "Last claimed time is not valid"
+            );
+            //New beneficiary's initial amount must be greater than 0
+            require(
+                _vestingDetails[i].initialAmount > 0,
+                "Initial release amount is not valid"
+            );
+            //New beneficiary's initial claimed must be false
+            require(
+                _vestingDetails[i].initialClaimed == false,
+                "Initial claimed is not valid"
+            );
+            //New beneficiary's claim start time must be not be before start date
+            require(
+                _vestingDetails[i].claimStartTime >= startDate,
+                "Claim start time is not valid"
+            );
+
+            vestingDetails[beneficiary] = VestingDetail(
+                beneficiary,
+                _vestingDetails[i].vestingAmount,
+                _vestingDetails[i].duration,
+                _vestingDetails[i].claimedAmount,
+                _vestingDetails[i].lastClaimedTime,
+                _vestingDetails[i].initialAmount,
+                _vestingDetails[i].initialClaimed,
+                _vestingDetails[i].claimStartTime
+            );
+
+            totalVestingAmount += _vestingDetails[i].vestingAmount;
+
+            emit Vested(beneficiary, _vestingDetails[i].vestingAmount);
         }
-        return _vest(who, claimedAmount);
     }
 
-    function _vest(address who, uint256 amount) private returns (bool) {
-        require(amount > 0, "Cannot claim 0 tokens");
+    /**
+     * @dev Allow user to claim token from vesting after start date.
+     */
+    function claim() override external {
+        //Start date must be must be in the past but not 0
+        require(
+            startDate > 0 && block.timestamp >= startDate,
+            "Claim is not allowed before start"
+        );
 
-        lastClaim[who] = block.timestamp;
-        claimed[who] += amount;
-        ERC20(token).safeTransfer(who, amount);
+        address beneficiary = msg.sender;
+        require(
+            block.timestamp >= vestingDetails[beneficiary].claimStartTime,
+            "Claim is not allowed before claim start date"
+        );
+        //Beneficiary must have a vesting amount
+        require(
+            vestingDetails[beneficiary].vestingAmount > 0,
+            "Vesting does not exist"
+        );
+        //Beneficiary must have not claimed all of their vesting amount
+        require(
+            vestingDetails[beneficiary].claimedAmount <
+                vestingDetails[beneficiary].vestingAmount,
+            "You have already claimed your vested amount"
+        );
 
-        return true;
+        uint256 amountToClaim;
+
+        // if initial claim is not done, claim initial amount + linear amount
+        if (
+            !vestingDetails[beneficiary].initialClaimed &&
+            vestingDetails[beneficiary].initialAmount > 0
+        ) {
+            amountToClaim += vestingDetails[beneficiary].initialAmount;
+            vestingDetails[beneficiary].initialClaimed = true;
+        }
+
+        // Check that block is after the cliff period.
+        if (block.timestamp >= vestingDetails[beneficiary].claimStartTime) {
+            uint256 lastClaimedTime = vestingDetails[beneficiary]
+                .lastClaimedTime;
+            if (lastClaimedTime == 0)
+                lastClaimedTime = vestingDetails[beneficiary].claimStartTime;
+
+            amountToClaim +=
+                ((block.timestamp - lastClaimedTime) *
+                    (vestingDetails[beneficiary].vestingAmount -
+                        vestingDetails[beneficiary].initialAmount)) /
+                vestingDetails[beneficiary].duration;
+
+            // In case the last claim amount is greater than the remaining amount
+            if (
+                amountToClaim >
+                vestingDetails[beneficiary].vestingAmount -
+                    vestingDetails[beneficiary].claimedAmount
+            )
+                amountToClaim =
+                    vestingDetails[beneficiary].vestingAmount -
+                    vestingDetails[beneficiary].claimedAmount;
+        }
+
+        vestingDetails[beneficiary].lastClaimedTime = block.timestamp;
+        vestingDetails[beneficiary].claimedAmount += amountToClaim;
+        totalClaimed += amountToClaim;
+        ERC20(token).safeTransfer(beneficiary, amountToClaim);
+
+        emit Claimed(beneficiary, amountToClaim);
     }
 
-    function claim() external returns (bool) {
+    /**
+     * @dev Get beneficiary's vesting detail
+     */
+    function getBeneficiaryVesting(address _beneficiary)
+        external
+        view
+        returns (VestingDetail memory)
+    {
+        return vestingDetails[_beneficiary];
+    }
 
-        // Vesting for funding_seed1
-        if (
-            msg.sender == funding_seed1 &&
-            lastClaim[funding_seed1] + funding_seed1VestingTime < block.timestamp
-        ) {
-            return _vestCalc(funding_seed1, funding_seed1InitVesting, funding_seed1VestingAmount, funding_seed1VestingCap);
-        }
+    /**
+     * @dev Allow owner to set operator
+     * @param _operator Operator address
+     */
+    function setOperator(address _operator) external onlyOwner {
+        require(_operator != address(0), "Address cannot be zero");
+        require(_operator != msg.sender, "Cannot set self as operator");
+        require(_operator != operator, "Already set");
+        operator = _operator;
+    }
 
-        // Vesting for paulAntonelli
-        if (
-            msg.sender == paulAntonelli &&
-            lastClaim[paulAntonelli] + paulAntonelliVestingTime < block.timestamp
-        ) {
-            return _vestCalc(paulAntonelli, paulAntonelliInitVesting, paulAntonelliVestingAmount, paulAntonelliVestingCap);
-        }
-
-        // Vesting for incentives
-        if (
-            msg.sender == incentives &&
-            lastClaim[incentives] + incentivesVestingTime < block.timestamp
-        ) {
-            return _vestCalc(incentives, incentivesInitVesting, incentivesVestingAmount, incentivesVestingCap);
-        }
-
-        // Vesting for devs
-        if (
-            msg.sender == dev &&
-            lastClaim[dev] + devVestingTime < block.timestamp
-        ) {
-            return _vestCalc(dev, devInitVesting, devVestingAmount, devVestingCap);
-        }
-
-        // Vesting for treasury
-        if (
-            msg.sender == treasury &&
-            lastClaim[treasury] + treasuryVestingTime < block.timestamp
-        ) {
-            return _vestCalc(treasury, treasuryInitVesting, treasuryVestingAmount, treasuryVestingCap);
-        }
-
-        // Vesting for team
-        if (
-            msg.sender == team &&
-            lastClaim[team] + teamVestingTime < block.timestamp
-
-        ) {
-            return _vestCalc(team, teamInitVesting, teamVestingAmount, teamVestingCap);
-        }
-
-        // Vesting for future
-        if (
-            msg.sender == future &&
-            lastClaim[future] + futureVestingTime < block.timestamp
-        ) {
-            return _vestCalc(future, futureInitVesting, futureVestingAmount, futureVestingCap);
-        }
-
-        // Vesting for intExpansion
-        if (
-            msg.sender == intExpansion &&
-            lastClaim[intExpansion] + intExpansionVestingTime < block.timestamp
-        ) {
-            return _vestCalc(intExpansion, intExpansionInitVesting, intExpansionVestingAmount, intExpansionVestingCap);
-        }
-        return false;
+    /**
+     * @dev Allow owner to set start date
+     * @param _date  The date of the start
+     */
+    function setStartDate(uint256 _date) external onlyOwner {
+        require(_date > block.timestamp, "Start date is not valid");
+        require(startDate != _date, "Start date is already set");
+        startDate = _date;
+        emit StartDateSet(_date);
     }
 }
